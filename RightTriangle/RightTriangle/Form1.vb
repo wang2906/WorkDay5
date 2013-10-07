9f6a657815a0d7c886f8e3793217335bca41c133
idsc3101 <idsc3101@gmail.com>
2013-10-07 15:20:14 -0500

    i solved

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,20 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
-
-        txtHyp.Text = "Not Implemented Yet"
+        Dim x As Double
+        Dim y As Double
+        Dim z As Double
+        x = CDbl(txtSideOne.Text)
+        y = CDbl(txtSideTwo.Text)
+        z = Hypotenuse(x, y)
+        txtHyp.Text = CStr(z)
 
     End Sub
+
+    Function Hypotenuse(ByVal a As Double, b As Double) As Double
+        Dim c As Double
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+        Return c
+    End Function
 
 End Class
